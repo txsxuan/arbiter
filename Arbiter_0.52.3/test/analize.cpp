10e31859873f13760e4d838ef0c6b81c2e186408
Yukini_Sakuhana <22050537@hdu.edu.cn>
2025-06-03 16:31:01 +0800

    正式戒扫，以后要玩除非从这里copy回来。

---
+++ Arbiter_0.52.3/test/analize.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// #include <fstream>
+int main(int argc, char *argv[]){
+    for(int i=0;i<argc;i++){
+        std::cout<<"asda";
+        std::cout<<"args : "<<argv[i]<<std::endl;
+    }
+    return 0;
+}
